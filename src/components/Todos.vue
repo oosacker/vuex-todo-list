7c5df61dd595fd1627d182f78d56c48539b64442
Natsuki Hasegawa <oosacker@gmail.com>
2021-05-11 23:42:39 +1200

    finish

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
       <h3>TODO LIST</h3>
+      <div class="legend">
+        <span>Double click to mark as complete</span>
+        <span>
+          <span class="incomplete-box"></span> = Incomplete
+        </span>
+        <span>
+          <span class="complete-box"></span> = Complete
+        </span>
+      </div>
       <div class="todos">
         <div 
           v-for="todo in allTodos" 
           :key="todo.id" 
           class="todo"
+          @dblclick="onDoubleClick(todo)"
+          :class="{'is-complete': todo.completed}"
         >
           {{ todo.title }}
           <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
@@ -20,11 +31,20 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
   name: "Todos",
   computed: mapGetters(["allTodos"]),
-  methods: {
+  methods: {// use spread operator for multiple actions
     ...mapActions([
         'fetchTodos',
-        'deleteTodo'
-      ]) // use spread operator for multiple actions
+        'deleteTodo',
+        'updateTodo'
+      ]),
+    onDoubleClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      }
+      this.updateTodo(updTodo)
+    }
   },
   created() {
     this.fetchTodos()
@@ -55,5 +75,36 @@ i {
   right: 10px;
   color: white;
   cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -31,13 +31,42 @@ const actions = {
     console.log(response)
 
     commit('removeTodo', id)
+  },
+  async filterTodos({ commit },  e) {
+    console.log(e)
+    const limit =  parseInt(e.target.options[e.target.options.selectedIndex].innerText)
+    console.log(limit)
+
+
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+    );
+    console.log(response)
+
+    // not having this code below seems to cause crashing: 
+    // vue.runtime.esm.js?2b0e:619 [Vue warn]: Error in v-on handler (Promise/async): "TypeError: Cannot read property 'options' of undefined"
+    commit('setTodos', response.data);
+  },
+  async updateTodo({ commit }, updTodo) {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, 
+      updTodo
+    );
+    console.log(response)
+    commit('updateTodo', response.data);
   }
 }
 
 const mutations = {
   setTodos: (state, todos) => state.todos = todos,
   newTodo: (state, todo) => state.todos.unshift(todo), // add elements to the beginning of the array
-  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
+  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+  updateTodo: (state, updTodo) => {
+    const index = state.todos.findIndex(todo => todo.id === updTodo.id)
+    if(index !== -1) {
+      state.todos.splice(index, 1, updTodo) // inserts updTodo at position index and removes old item at same position
+    }
+  }
 }
 
 export default {
